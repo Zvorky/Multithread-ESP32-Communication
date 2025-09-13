faa549baaa1b09b7465390a3c90608a4ca7c61dd
zvorky <enzoone@outlook.com>
2025-09-13 10:20:43 -0300

    Setup WiFi

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,18 +1,47 @@
-#include <Arduino.h>
+// Multithread ESP32 Communication - Client
 
-// put function declarations here:
-int myFunction(int, int);
+
+/* ─                     · ─ +
+ |  Enzo Zavorski Delevatti  |
+ .  @Zvorky                  ·
+             ___,
+          .~´    `-,
+         /  _    _ \.        .
+        ,\`|_|''|_|´\
+ .       /          /)       |
+        (\  ,    , .\`
+ |       `) ;`,; `,^,)
+         ´,´  `,  `  `
+        
+ |__ _  September 2025    _  |
+/                           */
+
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include "utils.h"
+
+
+// Leave blank to get user input OR set a default wifi
+const String ssid = "";
+const String passwd = "";
+
+WiFiClient tcp;
+WiFiUDP udp;
+
+String host = "";
+uint16_t udp_port = 1337;
+uint16_t tcp_port = 4269;
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  while (setupWiFi(ssid, passwd) != WL_CONNECTED) delay(1000);
+  Serial.println("\nStarting Client...");
 }
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println("TODO");
+  delay(1000);
 }
